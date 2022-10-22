2befd5843f9ff77b7f65fc8c33e692182b3cb04c
R. Richards <rdrrichards@hotmail.com>
2022-10-22 14:07:22 -0500

    interesting updates

--- QIQO.Code.Generator/DataAccessLayerGenerator.cs
+++ QIQO.Code.Generator/DataAccessLayerGenerator.cs
@@ -1,35 +1,83 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.IO;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace QIQO.Code.Generator
 {
-	[Generator]
-	public class DataAccessLayerGenerator : ISourceGenerator
-	{
-		public void Execute(GeneratorExecutionContext context)
-		{
-			var outout = @"public class ProductMapper {
+    [Generator]
+    public class DataAccessLayerGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var outout = CreateMapClass().ToFullString(); //.ReplaceLineEndings("");
+            context.AddSource("ProductMap.g.cs", outout);
+        }
 
-}";
-			context.AddSource("ProductMapper.g.cs", outout);
-		}
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new MainSyntaxReceiver());
+        }
+        private CompilationUnitSyntax CreateMapClass()
+        {
+            return CompilationUnit()
+                .WithUsings(
+                    List<UsingDirectiveSyntax>(
+                        new UsingDirectiveSyntax[]{
+                            UsingDirective(
+                                QualifiedName(
+                                    QualifiedName(
+                                        IdentifierName("QIQO"),
+                                        IdentifierName("Business")),
+                                    IdentifierName("Core"))),
+                            UsingDirective(
+                                IdentifierName("System")),
+                            UsingDirective(
+                                QualifiedName(
+                                    QualifiedName(
+                                        IdentifierName("System"),
+                                        IdentifierName("Collections")),
+                                    IdentifierName("Generic"))),
+                            UsingDirective(
+                                QualifiedName(
+                                    IdentifierName("System"),
+                                    IdentifierName("Data"))),
+                            UsingDirective(
+                                QualifiedName(
+                                    QualifiedName(
+                                        IdentifierName("System"),
+                                        IdentifierName("Data")),
+                                    IdentifierName("SqlClient")))}))
+                .WithMembers(
+                    SingletonList<MemberDeclarationSyntax>(
+                        ClassDeclaration("ProductMap")
+                        .WithModifiers(
+                            TokenList(
+                                Token(SyntaxKind.PublicKeyword)))
+                        .WithBaseList(
+                            BaseList(
+                                SeparatedList<BaseTypeSyntax>(
+                                    new SyntaxNodeOrToken[]{
+                                        SimpleBaseType(
+                                            IdentifierName("MapperBase")),
+                                        Token(SyntaxKind.CommaToken),
+                                        SimpleBaseType(
+                                            IdentifierName("IProductMap"))})))))
+                .NormalizeWhitespace();
 
-		public void Initialize(GeneratorInitializationContext context)
-		{
-			context.RegisterForSyntaxNotifications(() => new MainSyntaxReceiver());
-		}
-	}
+        }
+    }
 
-	public class MainSyntaxReceiver : ISyntaxReceiver
-	{
-		public int Index { get; set; }
-		public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-		{
-			if (syntaxNode is ClassDeclarationSyntax)
-			{
-				File.WriteAllText($@"C:\DEV\Source\RaD\QIQO.Code.Generator\QIQO.Code.Generator\{Index++}.txt", syntaxNode.GetText().ToString());
-			}
-		}
-	}
+    public class MainSyntaxReceiver : ISyntaxReceiver
+    {
+        public int Index { get; set; }
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax)
+            {
+                File.WriteAllText($@"C:\DEV\Source\RaD\QIQO.Code.Generator\QIQO.Code.Generator\{Index++}.txt", syntaxNode.GetText().ToString());
+            }
+        }
+    }
 }
