931df8bdfabf1ac196dacd1fe6b99c0df69701a9
R. Richards <rdrrichards@hotmail.com>
2022-10-22 12:56:38 -0500

    Add project files.

---
+++ QIQO.Code.Generator/DataAccessLayerGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
+
+namespace QIQO.Code.Generator
+{
+	[Generator]
+	public class DataAccessLayerGenerator : ISourceGenerator
+	{
+		public void Execute(GeneratorExecutionContext context)
+		{
+			var outout = @"public class ProductMapper {
+
+}";
+			context.AddSource("ProductMapper.g.cs", outout);
+		}
+
+		public void Initialize(GeneratorInitializationContext context)
+		{
+			context.RegisterForSyntaxNotifications(() => new MainSyntaxReceiver());
+		}
+	}
+
+	public class MainSyntaxReceiver : ISyntaxReceiver
+	{
+		public int Index { get; set; }
+		public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+		{
+			if (syntaxNode is ClassDeclarationSyntax)
+			{
+				File.WriteAllText($@"C:\DEV\Source\RaD\QIQO.Code.Generator\QIQO.Code.Generator\{Index++}.txt", syntaxNode.GetText().ToString());
+			}
+		}
+	}
+}
---
+++ QIQO.Data.Access/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿
+namespace QIQO.Data.Access.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Descript { get; set; }
+
+    }
+}
